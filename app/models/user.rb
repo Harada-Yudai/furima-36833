e3e51f0d92676f95269cdeadc16fe0c13ce711b2
Harada-Yudai <y.harada.dm@gmail.com>
2021-11-09 21:46:25 +0900

    エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  # validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_kana, presence: true
-  validates :first_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' }
+  validates :last_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/u, message: 'には全角カタカナを使用してください' }
+  validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/u, message: 'には全角カタカナを使用してください' }
   validates :birthday, presence: true
 
   has_many :items
