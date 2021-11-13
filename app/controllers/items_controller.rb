9264f3911bc578c057cf874013c56d080f253e6f
Harada-Yudai <y.harada.dm@gmail.com>
2021-11-13 15:33:53 +0900

    カテゴリー選択追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @iyem = Item.new
+  end
+
+  def create
+  end
+
+
+  private
+
+def prototype_params
+  params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :ship_pay_id,:prefecture_id,:ship_day_id).merge(user_id: current_user.id)
 end
+end
+ 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,25 @@
 class Item < ApplicationRecord
 
-
-
+  validates :name          , presence: true
+  validates :info          , presence: true
+  validates :category_id   , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id     , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :ship_pay_id   , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id , numericality: { other_than: 0 , message: "can't be blank"} 
+  validates :ship_day_id   , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price         , presence: true
+  validates :price         , presence: true
+  validates :image         , presence: true
 
   # has_one :buylog
   belongs_to :user
+  has_one_attached :image
+  has_many :cates
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :ship_pay
+  belongs_to :prefecture
+  belongs_to :ship_day
 end
