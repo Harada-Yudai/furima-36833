84656199a9fae55a303b58e6e84cdd86bba4a6f9
Harada-Yudai <y.harada.dm@gmail.com>
2021-11-15 22:28:25 +0900

    商品詳細表示機能(soldout以外)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -15,6 +16,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
